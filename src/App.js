8ad5fe2f93b5823bcb82947b837e414312b11d30
NguyenCatTuong1703 <nctedm@gmail.com>
2021-09-01 19:53:55 +0700

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Chat from './component/chat';
+import SharedFlies from './component/sharedfiles'
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+      <SharedFlies></SharedFlies>
+    </div>
+  );
+}
+
+export default App;
